1589b5d07834aad760b5d7d619cad778c261545e
Artem <artmnglv@gmail.com>
2020-10-10 17:08:57 +0500

    add styles for input

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -3,15 +3,32 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-input',
   template: `
-    <input #input [value]="value" (keyup)="sendValue(input.value)" />
+    <div class="field">
+      <label class="field__label" [class.field__label_shown]="value.length">{{
+        placeholder
+      }}</label>
+      <input
+        class="field__input"
+        #input
+        [value]="value"
+        (keyup)="sendValue(input.value)"
+        (keyup)="setValue(input.value)"
+        [placeholder]="placeholder"
+      />
+    </div>
   `,
   styleUrls: ['./input.component.scss'],
 })
 export class InputComponent {
   @Input() value = '';
+  @Input() placeholder = '';
   @Output() changeValueEvent = new EventEmitter();
 
   sendValue(value: string) {
     this.changeValueEvent.emit(value);
+  }
+
+  setValue(value: string) {
+    this.value = value;
   }
 }
