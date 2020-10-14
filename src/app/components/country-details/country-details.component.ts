a5fdb32c7e2d38ffb7cc1ad24ca1258d3696cc8f
Artem <artmnglv@gmail.com>
2020-10-14 11:45:03 +0500

    implement details page

--- src/app/components/country-details/country-details.component.ts
+++ src/app/components/country-details/country-details.component.ts
@@ -10,6 +10,13 @@ import { CountryDetailsGQL } from 'src/app/services/countriesGraphql.service';
 })
 export class CountryDetailsComponent implements OnInit {
   name: string;
+  native: string;
+  capital: string;
+  phone: string;
+  continent: string;
+  currency: string;
+  languages: string;
+  states: string;
 
   constructor(
     private route: ActivatedRoute,
@@ -25,6 +32,36 @@ export class CountryDetailsComponent implements OnInit {
       )
       .subscribe((country) => {
         this.name = country.name;
+        this.native = country.native;
+
+        this.capital = country.capital
+          ? country.capital
+          : 'There is no capital';
+
+        this.phone = country.phone;
+        this.continent = country.continent.name;
+
+        this.currency = country.currency
+          ? country.currency
+          : 'There is no currency';
+
+        const languages = country.languages.map((lang) => lang.name).join(', ');
+        this.languages = languages ? languages : 'There are no languages';
+
+        const states = country.states.map((state) => state.name).join(', ');
+        this.states = states ? states : 'There are no states';
       });
+  }
+
+  getProperties() {
+    return new Map([
+      ['Native name', this.native],
+      ['Capital', this.capital],
+      ['Phone prefix', this.phone],
+      ['Continent', this.continent],
+      ['Currency', this.currency],
+      ['Languages', this.languages],
+      ['States', this.states],
+    ]);
   }
 }
