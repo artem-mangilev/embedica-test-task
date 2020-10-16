1abd39601bf49028fddd161887c19cc2f935a077
Artem <artmnglv@gmail.com>
2020-10-16 09:05:49 +0500

    change naming in dropdown

--- src/app/components/dropdown/dropdown.component.ts
+++ src/app/components/dropdown/dropdown.component.ts
@@ -11,20 +11,20 @@ export class DropdownComponent implements OnInit {
   @Input() checkboxes: Checkbox[] = [];
   @Output() checkboxUpdateEvent = new EventEmitter();
 
-  selected = 0;
+  checked = 0;
   isDropdownShown = false;
 
   ngOnInit() {
     // ? checkboxes is undefined at start, but it should be an empty array by default
     if (this.checkboxes) {
-      this.selected = this.checkboxes.filter(
+      this.checked = this.checkboxes.filter(
         (checkbox) => checkbox.checked
       ).length;
     }
   }
 
   handleCheckboxClick(event: Checkbox) {
-    event.checked ? this.selected++ : this.selected--;
+    event.checked ? this.checked++ : this.checked--;
 
     this.checkboxUpdateEvent.emit(event);
   }
